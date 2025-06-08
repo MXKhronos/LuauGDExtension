8f65bcb11864a8f2cb3a2bda3b9f2bbe55f1315b
MXKhronos <38783502+MXKhronos@users.noreply.github.com>
2025-06-09 07:13:26 +0800

    Luau syntax highlighter is now registered into script editor in Godot.

--- src/luauscript/luauscript_syntax_highlighter.cpp
+++ src/luauscript/luauscript_syntax_highlighter.cpp
@@ -1,18 +1,27 @@
 
 #include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/text_edit.hpp>
 #include "luauscript_syntax_highlighter.h"
 #include "luau_constants.h"
 
 using namespace godot;
 
-Dictionary godot::LuauSyntaxHighlighter::_get_line_syntax_highlighting(int32_t p_line) const {
+Dictionary LuauSyntaxHighlighter::_get_line_syntax_highlighting(int32_t p_line) const {
+    
     return Dictionary();
 }
 
-void godot::LuauSyntaxHighlighter::_clear_highlighting_cache() {
+void LuauSyntaxHighlighter::_clear_highlighting_cache() {
 }
 
 void LuauSyntaxHighlighter::_update_cache() {
+    TextEdit* text_edit = get_text_edit();
+
+	font_color = text_edit->get_theme_color("text_editor/theme/highlighting/function_color"); //font_color
+	symbol_color = text_edit->get_theme_color("text_editor/theme/highlighting/symbol_color");
+	function_color = text_edit->get_theme_color("text_editor/theme/highlighting/function_color");
+	number_color = text_edit->get_theme_color("text_editor/theme/highlighting/number_color");
+
 }
 
 String LuauSyntaxHighlighter::_get_name() const { 
@@ -29,4 +38,4 @@ Ref<EditorSyntaxHighlighter> LuauSyntaxHighlighter::_create() const {
     Ref<LuauSyntaxHighlighter> highlighter;
     highlighter.instantiate();
     return highlighter;
-};
+}
