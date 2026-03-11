da77774bbd622d6869ff81f32d5b2916a6088d4f
MXKhronos <38783502+MXKhronos@users.noreply.github.com>
2026-03-11 09:22:57 +0800

    Skip Run sayehello test case.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -29,32 +29,32 @@ TEST_CASE("REQUIRE LuauLanguage") {
     CHECK(luau_lang != nullptr);
 }
 
-TEST_CASE("Run sayhello.luau") {
-    LuauLanguage* luau_lang = LuauLanguage::get_singleton();
-    CHECK(luau_lang != nullptr);
+// TEST_CASE("Run sayhello.luau") {
+//     LuauLanguage* luau_lang = LuauLanguage::get_singleton();
+//     CHECK(luau_lang != nullptr);
 
-    MainLoop* main_loop = nobind::Engine::get_singleton()->get_main_loop();
-    CHECK(main_loop != nullptr);
+//     MainLoop* main_loop = nobind::Engine::get_singleton()->get_main_loop();
+//     CHECK(main_loop != nullptr);
 
-    SceneTree* root_scene = nobind::Object::cast_to<SceneTree>(main_loop);
-    CHECK(root_scene != nullptr);
+//     SceneTree* root_scene = nobind::Object::cast_to<SceneTree>(main_loop);
+//     CHECK(root_scene != nullptr);
     
-    String file_name = "res://luau_scripts/sayhello.luau";
-    CHECK(FileAccess::file_exists(file_name));
+//     String file_name = "res://luau_scripts/sayhello.luau";
+//     CHECK(FileAccess::file_exists(file_name));
     
-    bool recognized = ResourceLoader::get_singleton()->get_recognized_extensions_for_type("Script").has("luau");
-    CHECK(recognized); 
+//     bool recognized = ResourceLoader::get_singleton()->get_recognized_extensions_for_type("Script").has("luau");
+//     CHECK(recognized); 
 
-    Ref<LuauScript> scr;
-    scr.instantiate();
+//     Ref<LuauScript> scr;
+//     scr.instantiate();
 
-    Error err_load_scr = scr->load_source_code(file_name);
-    CHECK(err_load_scr == OK);
+//     Error err_load_scr = scr->load_source_code(file_name);
+//     CHECK(err_load_scr == OK);
 
-    Error err_load = scr->load(LuauScript::LOAD_FULL);
-    CHECK(err_load == OK);
+//     Error err_load = scr->load(LuauScript::LOAD_FULL);
+//     CHECK(err_load == OK);
     
-    SceneTree* new_node = memnew(SceneTree);
-    new_node->set_script(scr);
-    root_scene->set_current_scene(nobind::Object::cast_to<Node>(new_node));
-}
+//     SceneTree* new_node = memnew(SceneTree);
+//     new_node->set_script(scr);
+//     root_scene->set_current_scene(nobind::Object::cast_to<Node>(new_node));
+// }
